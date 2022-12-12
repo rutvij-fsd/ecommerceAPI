fce6b671a2a27ad9201cd2582f0c4e0aafd11d7b
rutvij-fsd <rutvijpatel414@gmail.com>
2022-12-12 20:48:15 +0530

    Add Update API

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,3 +17,37 @@ exports.getAllProducts = async (req, res) => {
     product,
   });
 };
+
+//Delete a product
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  await product.remove();
+
+  res.status(200).json({ success: true, message: "Product deleted" });
+};
+
+//Update a product
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({ success: true, message: "Product updated" });
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,11 +2,14 @@ const express = require("express");
 const {
   getAllProducts,
   createProduct,
+  deleteProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/create").post(createProduct);
+router.route("/products/:id").put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
